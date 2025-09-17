76be90d1bb4eacfaeb4c0fd0e54afa232c70d423
2501074 <2501074@kbu.ac.kr>
2025-09-17 11:37:45 +0900

    week03

---
+++ week03/Sample3-1/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;  //선언문
+        float b = 0;
+        long c = 0; // c = @L~~
+        byte d = 0;
+        short f = 0;
+        boolean t = false;
+        char g = ' ';
+
+        final float PI = 3.141592f; // Symbolic Constants 기호적 상수
+
+
+        String y = null;
+        // String z = new String("");
+
+        PI = 3.14f;
+
+        a = 5;  // 대입문 5 정수형 리터형
+        b = 5.5f;   //5.5f는 float 리터럴 5.5 double literal
+        c = 5;
+
+        a = (int) 6L;    //Cast 연산자
+        d = (byte) -128;
+        f = (short) 32768;
+
+
+
+
+
+
+
+    }
+}
